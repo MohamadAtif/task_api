0d5ee0d0772ec81c804094ddcb493103d65c6d61
MohamadAtif <137002849+MohamadAtif@users.noreply.github.com>
2023-08-06 19:26:08 +0300

    allTask

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_api/features/api_service.dart';
+
+import 'package:task_api/features/repo/homerebo_implem.dart';
+import 'package:task_api/spalsh.dart';
+import 'features/cubits/cubit/most_popular_cubit.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: (context) =>
+                MostPopularCubit(HomeReboImple(ApiService(Dio())))
+                  ..fetchMostPopularRebosotries())
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'taskApi',
+        home: Splash(),
+      ),
+    );
+  }
+}
